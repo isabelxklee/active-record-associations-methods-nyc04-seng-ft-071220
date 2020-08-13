f6e97e2905c9243741df1e1dc4b7c1c68329da3a
isabel k. lee <hello@isabelklee.co>
2020-08-13 16:54:17 -0400

    finsihed artist methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,23 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+
+    # iterate through all the song instances
+    # only pick out the ones that are associated to this artist (self)
+    # mutate the array to only be genres
+    # call uniq on that array
+
+    self.songs.map { |song| song.genre }.uniq.count
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ Artist.delete_all
 
 hotline_bling = Song.create(:name=>'Hotline Bling')
 thriller = Song.create(:name=>'Thriller')
+song3 = Song.create(:name=>'Some Shitty Drake Song')
+song4 = Song.create(:name=>'Some Other Shitty Drake Song')
 
 drake = Artist.create(:name=>'Drake')
 mj = Artist.create(:name=>'Michael Jackson')
@@ -12,10 +14,16 @@ rap = Genre.create(:name=>'Rap')
 pop = Genre.create(:name=>'Pop')
 
 hotline_bling.artist = drake
+song3.artist = drake
+song4.artist = drake
 thriller.artist = mj
 
 drake.songs << hotline_bling
+drake.songs << song3
+drake.songs << song4
 mj.songs << thriller
 
 pop.songs << thriller
+pop.songs << song3
+pop.songs << song4
 rap.songs << hotline_bling
